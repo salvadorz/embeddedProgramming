a0fa3bb20d729316c00fd7920aed44e5a08fb6ef
Salvador Zendejas <salvador.zengar@gmail.com>
2018-04-22 17:10:15 -0500

    Feature - Adding Handler to use a callback and max and min functions

--- sampleCodesC/Calc/calc.c
+++ sampleCodesC/Calc/calc.c
@@ -8,15 +8,36 @@
 
 #include "calc.h"
 
+uint8 u8fn_calHandler(uint8 a,uint8 b, u8ptrfn_t u8_calc_fn){
+	return (u8_calc_fn(a,b));
+}
+
 uint8 u8fn_sum(uint8 a, uint8 b){
 	return (a + b);
 }
+
 uint8 u8fn_sub(uint8 a, uint8 b){
 	return (a - b);
 }
+
 uint8 u8fn_mul(uint8 a, uint8 b){
 	return (a * b);
 }
+
 uint8 u8fn_div(uint8 a, uint8 b){
 	return (a / b);
+}
+
+uint8 u8fn_max(uint8 a, uint8 b){
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+uint8 u8fn_min(uint8 a, uint8 b){
+	if (a < b)
+		return a;
+	else
+		return b;
 }
--- sampleCodesC/Calc/calc.h
+++ sampleCodesC/Calc/calc.h
@@ -10,11 +10,15 @@
 
 #include "common.h"
 
+typedef uint8 (*u8ptrfn_t)(uint8, uint8);
+
+uint8 u8fn_calHandler(uint8,uint8, u8ptrfn_t);
+
 uint8 u8fn_sum(uint8, uint8);
 uint8 u8fn_sub(uint8, uint8);
 uint8 u8fn_mul(uint8, uint8);
 uint8 u8fn_div(uint8, uint8);
-
-typedef uint8 (*u8ptrfn_t)(uint8, uint8);
+uint8 u8fn_max(uint8, uint8);
+uint8 u8fn_min(uint8, uint8);
 
 #endif /* CALC_CALC_H_ */
