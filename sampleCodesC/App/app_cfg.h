fc34ca4284a016f25a10ca68929790eed5dc721a
Salvador Zendejas <salvador.zengar@gmail.com>
2018-04-22 17:13:06 -0500

    Feat Added a app cfg in order to defin the cases to run

---
+++ sampleCodesC/App/app_cfg.h
@@ -0,0 +1,21 @@
+/*
+ * app_cfg.h
+ *
+ *  Created on: 22/04/2018
+ *      Author: John
+ */
+
+#ifndef APP_APP_CFG_H_
+#define APP_APP_CFG_H_
+
+#include "common.h"
+
+#ifndef TEST_MATH
+//#define TEST_MATH
+#endif
+
+#ifndef TEST_PTR
+#define TEST_PTR
+#endif
+
+#endif /* APP_APP_CFG_H_ */
--- sampleCodesC/main.c
+++ sampleCodesC/main.c
@@ -6,20 +6,27 @@
  */
 
 #include "common.h"
+#include "App/app_cfg.h"
 #include "App/test_Math.h"
-
+#include "Pointer/pointer.h"
 
 int main(void) {
 
+#ifdef TEST_PTR
+	TEST_PRT();
+#endif
+
 	puts("!!!Hello World!!!");
 
+
+#ifdef TEST_MATH
 	if(test_Math() == PASS){
 		printf("TEST END SUCCESFUL\n");
 	}
 	else {
 		printf("TEST END WITH ERRORS\n");
 	}
-
+#endif
 
 
 }
